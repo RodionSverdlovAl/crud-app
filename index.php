92c0a909ffb82757957a5ce96451385be52bfd82
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-08-19 21:20:28 +0300

    add read

--- index.php
+++ index.php
@@ -1,2 +1,49 @@
 <?php
- phpinfo();
+require_once 'connect.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Products</title>
+</head>
+<style>
+    th,td{
+        padding:10px;
+        border: 2px solid black;
+    }
+    th{
+        background: #7cbf8f;
+    }
+</style>
+<body>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>description</th>
+            <th>price</th>
+        </tr>
+
+        <?php
+        $products = mysqli_query($connect, "SELECT * FROM `products`");
+        $products = mysqli_fetch_all($products);
+        foreach ($products as $product){
+            ?>
+                <tr>
+                    <td><?= $product[0]?></td>
+                    <td><?= $product[1]?></td>
+                    <td><?= $product[3]?></td>
+                    <td><?= $product[2]?></td>
+                </tr>
+            <?php
+        }
+        ?>
+
+    </table>
+
+</body>
+</html>
+
+
